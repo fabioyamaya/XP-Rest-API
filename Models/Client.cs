b33365487f2f094fb5d25efc0d693def75b2059a
fabio.yamaya <fabio.yamaya@gmail.com>
2023-10-23 21:00:23 -0300

    adds Address to ClientModel

--- Models/Client.cs
+++ Models/Client.cs
@@ -6,4 +6,6 @@ public class Client
   public required string Phone { get; set; }
   public required string Email { get; set; }
   public string? SecondaryEmail { get; set; }
+  public required string Address { get; set; }
+  public string? SecondaryAddress { get; set; }
 }
